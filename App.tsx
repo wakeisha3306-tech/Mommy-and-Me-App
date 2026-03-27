324deeebf9c3cbce8c81c6e383809f30a13fca0e
wakeisha3306-tech <wakeisha3306@gmail.com>
2026-03-26 21:48:02 -0400

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,39 @@
+import { Switch, Route, Router as WouterRouter } from "wouter";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+
+import Home from "@/pages/home";
+import Affirmations from "@/pages/affirmations";
+import Journal from "@/pages/journal";
+import Notes from "@/pages/notes";
+import NotFound from "@/pages/not-found";
+
+const queryClient = new QueryClient();
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/affirmations" component={Affirmations} />
+      <Route path="/journal" component={Journal} />
+      <Route path="/notes" component={Notes} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+          <Router />
+        </WouterRouter>
+        <Toaster />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
